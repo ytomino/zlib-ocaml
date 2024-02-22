2dae35e86c06111c963392489a95cc32b5de5145
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-02-23 06:11:00 +0900

    Add Zlib.is_inflated_out
    And improve the examples of mlzlib.

--- source/zlib.mli
+++ source/zlib.mli
@@ -42,5 +42,10 @@ val inflate_out: out_inflater -> string -> int -> int -> int
 val inflate_flush: out_inflater -> unit
 val inflate_end_out: out_inflater -> unit
 
+val is_inflated_out: out_inflater -> bool
+(** [true] means the inflating is finished internally.
+    It does not require any more calling of [inflate_out], but the data may be
+    remained, so [inflate_end_out] or [inflate_flush] should be called. *)
+
 val crc32_substring: int32 -> string -> int -> int -> int32
 val crc32_string: int32 -> string -> int32
--- source/zlib_stub.c
+++ source/zlib_stub.c
@@ -161,6 +161,7 @@ CAMLprim value mlzlib_deflate(
 		result = false;
 		break;
 	case Z_STREAM_END:
+		Store_field(val_fields, 6, Val_true);
 		result = true;
 		break;
 	default:
@@ -241,6 +242,7 @@ CAMLprim value mlzlib_inflate(
 		result = false;
 		break;
 	case Z_STREAM_END:
+		Store_field(val_fields, 6, Val_true);
 		result = true;
 		break;
 	default:
